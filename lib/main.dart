a5f03aa110bf6cc17354e05b7d40bbf5b7ab622b
Wazir Tatheer Abbas <baltiwazir7@gmail.comm>
2024-06-02 04:18:44 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,22 @@ import 'package:firstclass/profile_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ProfilesSreen(),
+      title: 'Personal Info',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Personal Info'),
+        ),
+        body: Center(
+          child: PersonalInfo(),
+        ),
+      ),
     );
   }
 }
---
+++ lib/personal_info.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class PersonalInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Name: Wazir Tatheer Abbas',
+          style: TextStyle(fontSize: 20.0),
+        ),
+        Text(
+          'Skills: Flutter, Dart, Mobile Development',
+          style: TextStyle(fontSize: 20.0),
+        ),
+        Text(
+          'Hobbies: Reading, Hiking, Playing Guitar',
+          style: TextStyle(fontSize: 20.0),
+        ),
+        Text(
+          'Profession: Software Engineer',
+          style: TextStyle(fontSize: 20.0),
+        ),
+      ],
+    );
+  }
+}
